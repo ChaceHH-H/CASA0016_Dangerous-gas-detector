06b6d1b6e614064cfc6c29372b426bc99420036c
ChaceHH-H <146041784+ChaceHH-H@users.noreply.github.com>
2024-01-08 01:33:19 +0000

    Add files via upload

--- gas-1.ino
+++ gas-1.ino
@@ -3,20 +3,20 @@
 #include <U8g2lib.h>
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
-
 #define         Board                   ("Arduino UNO")
 #define         Pin4                     (A0)
 #define         Pin7                     (A1)
 #define         Pin8                     (A2)
 #define         Pin9                     (A3)
 
-#define         RatioMQ4CleanAir          (4.4)  //RS / R0 = 4.4 ppm
-#define         RatioMQ7CleanAir          (27.5) //RS / R0 = 27.5 ppm
-#define         RatioMQ8CleanAir          (70) //RS / R0 = 70 ppm
-#define         RatioMQ9CleanAir          (9.6) //RS / R0 = 9.6 ppm
-#define         ADC_Bit_Resolution        (10) // 10 bit ADC 
-#define         Voltage_Resolution        (5) //  Volt resolution to calc the voltage
-#define         Type                      ("Arduino UNO") //Board used
+#define         RatioMQ4CleanAir          (4.4)  
+#define         RatioMQ7CleanAir          (27.5) 
+#define         RatioMQ8CleanAir          (70) 
+#define         RatioMQ9CleanAir          (9.6) 
+#define         ADC_Bit_Resolution        (10) 
+#define         Voltage_Resolution        (5) 
+#define         Type                      ("Arduino UNO")
+//Declare sensor
 MQUnifiedsensor MQ4(Board, Voltage_Resolution,  ADC_Bit_Resolution, Pin4, Type);
 MQUnifiedsensor MQ7(Board, Voltage_Resolution,  ADC_Bit_Resolution, Pin7, Type);
 MQUnifiedsensor MQ8(Board, Voltage_Resolution,  ADC_Bit_Resolution, Pin8, Type);
@@ -38,8 +38,9 @@ String fg;
 
 void setup(){
   Serial.begin(9600);
+  //Initialize mq sensor, set R0
   MQ4.init();
-  MQ4.setRegressionMethod(1);  //_PPM =  a*ratio^b
+  MQ4.setRegressionMethod(1);
   MQ4.setR0(3.19);
   MQ7.init();
   MQ7.setRegressionMethod(1);
@@ -51,6 +52,7 @@ void setup(){
   MQ9.setRegressionMethod(1);
   MQ9.setR0(0.99);
 
+  //Calibrate the R0 value of the sensor
   // Serial.print("Calibrating  please wait.");
   // float  MQ4calcR0 = 0,
   //        MQ7calcR0 = 0,
@@ -82,7 +84,7 @@ void setup(){
   // Serial.print(MQ8calcR0  / 10); Serial.print(" | ");
   // Serial.print(MQ9calcR0 / 10); Serial.println("  |");
 
-
+  //Initialize oled
   u8g2.begin();
   u8g2.setFont(u8g2_font_t0_11_tf);
   pinMode(buzzer,OUTPUT);
@@ -97,11 +99,9 @@ void loop() {
   MQ7.update();
   MQ8.update();
   MQ9.update();
-
-  MQ4.setA(1012.7); MQ4.setB(-2.786); //CH4
+  //Set up the exponential equation for the gas, the A and B values are PPM = A*ratio^B
+  MQ4.setA(1012.7); MQ4.setB(-2.786); 
   CH4 = MQ4.readSensor();
-  //MQ4.setA(30000000); MQ4.setB(-8.308);
-  //float smoke = MQ4.readSensor();
   MQ7.setA(99.042);  MQ7.setB(-1.518);
   CO = MQ7.readSensor();
   MQ8.setA(976.97);  MQ8.setB(-0.688);
@@ -109,18 +109,18 @@ void loop() {
   MQ9.setA(1000.5);  MQ9.setB(-2.186);
   FG = MQ9.readSensor();
 
+  //Display values on the monitor
   Serial.print("Methane:  "); Serial.println(CH4);
   Serial.print("CO:       "); Serial.println(CO);
   Serial.print("H2:       "); Serial.println(H2);
   Serial.print("FG:       "); Serial.println(FG);
-  Serial.println("--------------------------------------------------------");
 
-  //u8g2.firstPage();
   ch4="Methane: " + String(CH4);
   co="CO     : " + String(CO);
   h2="H2     : " + String(H2);
   fg="FG     : " + String(FG);
 
+  //Determination of security alarm，Start the LED light and buzzer
   if(CH4>1000||CO>50||H2>50||FG>20){
     detection();
     digitalWrite(Red_LED,HIGH);
@@ -128,7 +128,7 @@ void loop() {
     tone(buzzer, 1000);
   }else{
     u8g2.clearBuffer();
-    u8g2.drawStr(50,10,"SAVE");
+    u8g2.drawStr(50,10,"SAFE");
     u8g2.drawStr(0,20,ch4.c_str());
     u8g2.drawStr(0,30,co.c_str());
     u8g2.drawStr(0,40,h2.c_str());
@@ -138,21 +138,10 @@ void loop() {
     digitalWrite(Green_LED,HIGH);
     noTone(buzzer);
   }
-
-  // tone(buzzer, 1000);
-  // delay(1000);
-  // noTone(buzzer);
-
-  // digitalWrite(Red_LED,HIGH);
-  // delay(2000);
-  // digitalWrite(Red_LED,LOW);
-  // digitalWrite(Green_LED,HIGH);
-  // delay(2000);
-  // digitalWrite(Green_LED,LOW);
-
   delay(5000);
 }
 
+//Determination of security alarm，displayed on oled screen
 void detection(){
     u8g2.clearBuffer();
     u8g2.drawStr(0,10,"!Danger gas detected!");
